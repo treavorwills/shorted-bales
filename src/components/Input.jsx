30da3e4b024bd53be50d1f8ff03b1938728f5caa
twills <treavorwills@gmail.com>
2023-07-13 23:26:52 -0600

    input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,15 @@
+import React from 'react';
 import './Input.css'
 
 function Input() {
+    const handleFileInputChange = (event) => {
+      console.log('clicky click clack')
+    };
 
   return (
     <>
     <div>Input</div>
-    
+    <div><input type="file" onChange={handleFileInputChange} /></div>
     </>
   )
 }
