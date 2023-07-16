0b234410cc2520b0c161ad73549c7e4e33528e4b
twills <treavorwills@gmail.com>
2023-07-16 16:47:02 -0600

    fully transform data into customer object with object of orders, each order having an object of materials

--- src/helpers/transformShortedBaleData.js
+++ src/helpers/transformShortedBaleData.js
@@ -1,26 +1,63 @@
 export default function transformShortedBaleData(data) {
-    const transformedData = {};
-    const customersArray = {};
+  const customers = {};
 
-    // Create an array of customer numbers from this report
-    data.forEach((entry) => {
-        const {"Cust #": customerId, "Cust Name": name, Customer_PO: po, "TMS ID": tmsId, "PKZ ID": orderId, "Cust P/N": custPartNumber, "Material #": materialId, "Bale Width": width, Grade: grade, Flute: flute, "Print?": print, "Ordered Qty": qtyOrdered, "Shipped Qty": qtyShipped, "Shorted Qty": qtyShorted } = entry;
-        if (!customersArray[customerId]) {
-            customersArray[customerId] = {
-                customerId: customerId,
-                name: name,
-                orders: {},
-            };
-        }
-    });
+  // Create an array of customer numbers from this report
+  data.forEach((entry) => {
+    const {
+      "Cust #": customerId,
+      "Cust Name": name,
+      Customer_PO: po,
+      "TMS ID": tmsId,
+      "PKZ ID": orderId,
+      "Cust P/N": custPartNumber,
+      "Material #": materialId,
+      "Bale Width": width,
+      Grade: grade,
+      Flute: flute,
+      "Print?": print,
+      "Ordered Qty": qtyOrdered,
+      "Shipped Qty": qtyShipped,
+      "Shorted Qty": qtyShorted,
+    } = entry;
 
-    Object.entries(customersArray).forEach(([key, value]) => {
-        console.log("key and value: ", key, value);
-    })
+    if (!customers[customerId]) {
+      customers[customerId] = {
+        customerId: customerId,
+        name: name,
+        orders: {},
+      };
+    }
 
-    console.log("customersArray: ", customersArray);
-    // console.log("data in to transform function: ", data);
-    // console.log("data that has been transformed: ", transformedData);
-    // console.log("data type of transformed Data: ", typeof transformedData);
-    return transformedData;
+    const customer = customers[customerId];
+
+    if (!customer.orders[orderId]) {
+      customer.orders[orderId] = {
+        orderId: orderId,
+        po: po,
+        tmsId: tmsId,
+        materials: {},
+      };
+    }
+
+    const order = customer.orders[orderId];
+
+    order.materials[materialId] = {
+      materialId: materialId,
+      custPartNumber: custPartNumber,
+      qtyOrdered: qtyOrdered,
+      qtyShipped: qtyShipped,
+      qtyShorted: qtyShorted,
+      width: width,
+      flute: flute,
+      grade: grade,
+      print: print,
+    };
+  });
+
+  console.log("customers: ", customers);
+  // console.log("data in to transform function: ", data);
+  // console.log("data that has been transformed: ", transformedData);
+  // console.log("data type of transformed Data: ", typeof transformedData);
+  const transformedData = customers;
+  return transformedData;
 }
