41c258d2cd4962b01c6ae361ce6a1c0ad4e957c5
twills <treavorwills@gmail.com>
2023-08-15 22:40:27 -0600

    header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,18 @@ function Header() {
   return (
     <>
       <div className="top-hero">
-        {/* <img src="corrugated.jpg" alt="corrugated image" /> */}
+        <div className="credit">
+          <p className="photo-credit">
+            photo by
+            <a href="https://unsplash.com/@roberto_sorin" target="_blank">
+              Roberto Sorin on Unsplash
+            </a>
+          </p>
+        </div>
+        <div className="name">
+          <p>shorted bales</p>
+        </div>
       </div>
-      <h1>shorted bales</h1>
       <p>
         Upload the shorted bales CSV file, add or review the email contacts, and
         then hit send! Don't forget to add the "Shorted Bales Review" note to
